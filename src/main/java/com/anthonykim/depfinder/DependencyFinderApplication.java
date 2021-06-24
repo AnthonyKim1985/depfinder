2663c9500968bd89f9d57ef2e24cb8378cc96b11
Anthony Kim <anthony.jinhyuk.kim@gmail.com>
2021-06-24 15:40:07 +0900

    first commit

---
+++ src/main/java/com/anthonykim/depfinder/DependencyFinderApplication.java
@@ -0,0 +1,11 @@
+package com.anthonykim.depfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyFinderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyFinderApplication.class, args);
+    }
+}
---
+++ src/test/java/com/anthonykim/depfinder/DependencyFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthonykim.depfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
